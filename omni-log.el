907eb8f52bd7c40f650991249d17b15cdf5ed477
Adriean Khisbe <adriean.khisbe@live.fr>
2014-07-27 16:04:48 +0200

    init: skeleton

---
+++ omni-log.el
@@ -0,0 +1,35 @@
+;;; omni-log.el --- Logging utilities
+
+;; Copyright (C) 2014  Adrien Becchis
+;; Created:  2014-07-27
+;; Version: 0.1
+
+;; Author: Adrien Becchis <adriean.khisbe@live.fr>
+;; Keywords: convenience, languages, tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Building Notes:
+;; far too early [and pretentious] to call the 'the long lost logging api' ^^
+
+;;; Code:
+
+
+
+(provide 'omni-log)
+;;; omni-log.el ends here
