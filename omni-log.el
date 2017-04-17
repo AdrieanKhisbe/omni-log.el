3595d291c14a21878ab7e01e177b9812d33d488a
AdrieanKhisbe <adriean.khisbe@live.fr>
2017-04-17 21:16:05 +0200

    dev: bump dash version

--- omni-log.el
+++ omni-log.el
@@ -5,7 +5,7 @@
 ;; Author: Adrien Becchis <adriean.khisbe@live.fr>
 ;; Created:  2014-07-27
 ;; Version: 0.1.2
-;; Package-Requires: ((emacs "24") (ht "2.0") (s "1.6.1") (dash "1.8.0") )
+;; Package-Requires: ((emacs "24") (ht "2.0") (s "1.6.1") (dash "2.13.0") )
 ;; Url: https://github.com/AdrieanKhisbe/omni-log.el
 ;; Keywords: convenience, languages, tools
 
