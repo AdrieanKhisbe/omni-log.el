5528446f32b0da65eb57a4ddafb05d4d334745aa
AdrieanKhisbe <adriean.khisbe@live.fr>
2017-04-18 20:20:07 +0200

    test: add testing for the log method

--- test/omni-log-test.el
+++ test/omni-log-test.el
@@ -1,17 +1,28 @@
 
 ;; ¤> Logger Creation
-(ert-deftest omni-log-logger/from-logger()
+(ert-deftest omni-log-logger/from-logger ()
   (let ((logger '(logger "dummy-logger")))
     (should (eq logger (omni-log-logger logger)))))
 
 
-(ert-deftest can-create-logger()
+(ert-deftest can-create-logger ()
   (let ((test-logger (omni-log-create-logger "ansible")))
     (should (omni-log-logger-p test-logger))
     (should (equal "ansible" (omni-log-logger-name test-logger)))
     (omni-log-kill-logger test-logger)))
 
-(ert-deftest can-create-logger-with-properties()
+(ert-deftest cannot-recreate-logger ()
+  (let ((test-logger (omni-log-create-logger "ansible")))
+    (should (equal (omni-log-create-logger "ansible") "A logger named ansible already exists"))
+    (omni-log-kill-logger test-logger)))
+
+(ert-deftest can-retrieve-logger-by-name ()
+  (let ((test-logger (omni-log-create-logger "ansible")))
+    (should (equal (omni-log-logger "ansible") test-logger))
+    (omni-log-kill-logger test-logger)))
+
+
+(ert-deftest can-create-logger-with-properties ()
   (let ((test-logger (omni-log-create-logger "ansible" '((a . "1") (b . 2)))))
     (should (omni-log-logger-p test-logger))
     (should (equal "ansible" (omni-log-logger-name test-logger)))
@@ -19,20 +30,20 @@
     (omni-log-kill-logger test-logger)))
 
 
-(ert-deftest can-access-logger-with-properties()
+(ert-deftest can-access-logger-with-properties ()
   (let ((test-logger (omni-log-create-logger "ansible" '((a . "1") (b . 2)))))
     (should (equal '((a . "1") (b . 2)) (omni-log-logger-properties test-logger)))
     (should (equal (omni-log-logger-property test-logger 'a) "1"))
     (omni-log-kill-logger test-logger)))
 
-(ert-deftest can-access-logger-with-properties-with-default()
+(ert-deftest can-access-logger-with-properties-with-default ()
   (let ((test-logger (omni-log-create-logger "ansible" '((a . "1") (b . 2)))))
     (should (equal '((a . "1") (b . 2)) (omni-log-logger-properties test-logger)))
     (should (equal (omni-log-logger-property test-logger 'a 2) "1"))
     (should (equal (omni-log-logger-property test-logger 'c 2) 2))
     (omni-log-kill-logger test-logger)))
 
-(ert-deftest can-create-logger-and-function-in-one-call()
+(ert-deftest can-create-logger-and-function-in-one-call ()
   (let ((test-logger (omni-log-create "ansible" '((a . "1") (b . 2)))))
     (should (equal '((a . "1") (b . 2)) (omni-log-logger-properties test-logger)))
     (should (fboundp 'log-ansible))
@@ -45,25 +56,54 @@
     (should (equal '((a . 1) (b . 2) (c . 3)) (omni-log-logger-properties test-logger)))
     (omni-log-kill-logger test-logger)))
 
+;; ¤> Kill buffer
+
+;; §todo: kill buffer
+;; §todo: kill buffer with archive
+
+(ert-deftest kill-no-logger ()
+  (should-error
+   (omni-log-kill-logger "toto")
+   :type 'wrong-type-argument))
+
 ;; ¤> Logging
-(ert-deftest can-log-to-created-logger()
+(ert-deftest log-with-name ()
+  (with-logger "ansible"
+    (log "ansible" "Yo")
+    (should-log test-logger "Yo\n")))
+
+(ert-deftest log-with-logger ()
+  (with-logger "ansible"
+   (log test-logger "Yolo!")
+   (should-log test-logger "Yolo!\n")))
+
+(ert-deftest log-no-log ()
+  (should (equal (log "unknown" "Lost Bottle")
+                 "Warning (emacs): There is no logger of name unknown.")))
+
+(ert-deftest can-log-to-created-logger ()
   (let ((test-logger (omni-log-create-logger "ansible")))
     (omni-log-message-to-logger test-logger "42")
     (should-log test-logger "42\n")
     (omni-log-kill-logger test-logger)))
 
-(ert-deftest can-create-logger-method()
+(ert-deftest can-create-logger-method ()
   (with-logger "ansible"
     (log-ansible "Working!")
     (should-log test-logger "Working!\n")))
 
-(ert-deftest can-create-logger-method-with-variable-name()
+(ert-deftest cannot-recreate-logger-method ()
+  (with-logger "ansible"
+    (should (equal (omni-log-create-log-function test-logger)
+                   "Warning (emacs): log-ansible logging function has already been made!"))))
+
+(ert-deftest can-create-logger-method-with-variable-name ()
   (let ((name "yo"))
     (with-logger name
       (log-yo "Working!")
       (should-log test-logger "Working!\n"))))
 
-(ert-deftest can-log-with-rest-message()
+(ert-deftest can-log-with-rest-message ()
   (with-logger "ansible"
     (log-ansible "Working %s %d!" "on" 42)
     (should-log test-logger "Working on 42!\n")))
@@ -80,7 +120,7 @@
     (should-log test-logger "Toto\n")))
 
 ;; ¤> color utils and fading
-(ert-deftest color-gradient-name()
+(ert-deftest color-gradient-name ()
   (should (equal (omni-log-color-gradient-name "black" "white" 4)
                  '("black" "#333333" "#666666" "#999999" "#cccccc" "white"))))
 
